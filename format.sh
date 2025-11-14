33d9a35a101576cc5a719beeab6dc2397ff01c6a
Josh Parker <JoshWolfParker@gmail.com>
2025-11-14 16:07:41 -0700

    add eslint config for javascript

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-prettier -w ./script.js && prettier -w ./index.html && prettier -w style.css
+prettier -w ./script.js && prettier -w ./index.html && prettier -w style.css && npx eslint ./script.js
